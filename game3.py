697cacd160a7512ad041d688bf856ac02c08648e
Hiromitsu Yamaguchi <yamagame@mac.com>
2018-05-30 14:29:35 +0900

    move gunman to up down left right with cursor keys

--- game3.py
+++ game3.py
@@ -1,15 +1,19 @@
 import gamesys
 
 game = gamesys.init('GUNMAN', 30)
-gunmanUP = game.load(game.file('gunman01.png'))
-gunmanDOWN = game.load(game.file('gunman02.png'))
+gunman = game.load(game.file('gunman01.png'))
 
+x = 448
 y = 320
 
 while True:
+  if game.key(game.KEY_LEFT):
+    x = x - 4
+  if game.key(game.KEY_RIGHT):
+    x = x + 4
   if game.key(game.KEY_UP):
     y = y - 4
   if game.key(game.KEY_DOWN):
     y = y + 4
-  game.draw(gunmanUP, 448, y)
+  game.draw(gunmanUP, x, y)
   game.update()
